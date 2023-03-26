c39c145275472d7851376eea73128d748e22ea60
Sameer <sameerg2255@gmail.com>
2023-03-26 17:46:23 +0530

    basic lexer created

---
+++ src/lexer.rs
@@ -0,0 +1,156 @@
+use crate::definations::KNWON_SYMBOLS;
+use std::str::Chars;
+
+#[derive(Debug)]
+#[derive(Clone)]
+#[derive(Copy)]
+pub enum TokenType {
+    Number,
+    Text,
+    Symbol
+}
+#[derive(Debug)]
+pub struct Token {
+    token: String,
+    token_type: TokenType,
+    start_index: u32
+}
+
+impl Token {
+    pub fn new(token: String, token_type: TokenType, start_index: u32) -> Token {
+        Token{
+            token,
+            token_type,
+            start_index
+        }
+    }
+}
+
+fn is_numeric(c:char)->bool {
+    c as u8 >= 48 && c as u8 <=  57
+}
+fn is_alpha(c:char)->bool {
+    (c as u8 >= 65 && c as u8 <=  90) || (c as u8 >= 97 && c as u8 <=  122)
+}
+#[derive(Debug)]
+pub struct Lexer<'a> {
+    index:u32,
+    chars: Chars<'a>,
+    buffer: Vec<char>,
+    last_token_type: TokenType,
+    optional_char: Option<char>
+}
+impl Lexer<'_>{
+    pub fn new<'a>(expression: &'a str) ->Lexer<'a> {
+        Lexer {
+            chars: expression.chars(),
+            index: 0,
+            buffer: Vec::new(),
+            last_token_type: TokenType::Text,
+            optional_char: None
+        }
+    }
+    fn next(&mut self) {
+        self.optional_char = self.chars.next();
+        if self.optional_char.is_some() {
+            self.index = self.index + 1;
+        }
+    }
+    pub fn next_token(&mut self) -> Option<Token> {
+        if self.index == 0 { //Called first time
+            self.next();
+            if self.optional_char.is_none() {
+                return None;
+            }
+            let c: char = self.optional_char.unwrap();
+            if is_numeric(c) { //Number
+                self.last_token_type = TokenType::Number;
+                self.buffer.push(c)
+            } else if KNWON_SYMBOLS.contains(&c) { // Known symbols
+                self.last_token_type = TokenType::Symbol;
+                self.buffer.push(c)
+            } else { //Text
+                self.last_token_type = TokenType::Text;
+                self.buffer.push(c)
+            }
+        }
+        self.next();
+        let mut c: char;
+        while self.optional_char.is_some() {
+            c = self.optional_char.unwrap();
+            if is_numeric(c) { //Number
+                match self.last_token_type {
+                    TokenType::Number => self.buffer.push(c),
+                    TokenType::Text => {
+                        self.last_token_type = TokenType::Text;
+                        self.buffer.push(c)
+                    },
+                    TokenType::Symbol => {
+                        if self.buffer[self.buffer.len()-1] == '.' {
+                            self.buffer.push(c);    
+                        } else {
+                            let token = Token::new(self.buffer.iter().collect(), self.last_token_type, self.index-self.buffer.len() as u32 -1);
+                            self.buffer.clear();
+                            self.last_token_type = TokenType::Number;
+                            self.buffer.push(c);
+                            return Some(token)
+                        }
+                    },
+                }
+            } else if KNWON_SYMBOLS.contains(&c) { // Known symbols
+                match self.last_token_type {
+                    TokenType::Number => {
+                        if c == '.' && !self.buffer.contains(&'.') {
+                            self.buffer.push(c);    
+                        } else {
+                            let token = Token::new(self.buffer.iter().collect(), self.last_token_type, self.index-self.buffer.len() as u32 -1);
+                            self.buffer.clear();
+                            self.last_token_type = TokenType::Symbol;
+                            self.buffer.push(c);
+                            return Some(token)
+                        }
+                    },
+                    TokenType::Text => {
+                        let token = Token::new(self.buffer.iter().collect(), self.last_token_type, self.index-self.buffer.len() as u32 -1);
+                        self.buffer.clear();
+                        self.last_token_type = TokenType::Symbol;
+                        self.buffer.push(c);
+                        return Some(token)
+                    },
+                    TokenType::Symbol => {
+                        let token = Token::new(self.buffer.iter().collect(), self.last_token_type, self.index-self.buffer.len() as u32 -1);
+                        self.buffer.clear();
+                        self.last_token_type = TokenType::Symbol;
+                        self.buffer.push(c);
+                        return Some(token)
+                    },
+                }
+            } else { //Text
+                match self.last_token_type {
+                    TokenType::Number => {
+                        self.last_token_type = TokenType::Text;
+                        self.buffer.push(c);
+                    },
+                    TokenType::Text => {
+                        self.buffer.push(c);
+                    },
+                    TokenType::Symbol => {
+                        let token = Token::new(self.buffer.iter().collect(), self.last_token_type, self.index-self.buffer.len() as u32 -1);
+                        self.buffer.clear();
+                        self.last_token_type = TokenType::Text;
+                        self.buffer.push(c);
+                        return Some(token)
+                    },
+                }
+            }
+            self.next();
+        }
+        
+        if self.buffer.is_empty() {
+            return None;
+        }
+        let token = Token::new(self.buffer.iter().collect(), self.last_token_type, self.index-self.buffer.len() as u32 -1);
+        self.buffer.clear();
+        return Some(token)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod lexer;
+pub mod definations;
+
+
+use lexer::{Lexer, Token};
 fn main() {
-    println!("Hello, world!");
+    let test_expression = "AVERAGE(125,485,987,45,0,-80,70) + 100 & USD";
+    let mut lexer = Lexer::new(test_expression);
+    let mut token = lexer.next_token();
+    while token.is_some() {
+        println!("{:?}", token);
+        token = lexer.next_token();
+    }
 }
